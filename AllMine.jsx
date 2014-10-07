7c1822157a606f09ed9ccfb7a5db5accb7eb4e25
Kevin Bjorke <Kevin@admins-MacBook-Air-8.local>
2014-10-07 01:46:45 -0700

    Refactor, break-out cameras and aspect ratio

--- AllMine.jsx
+++ AllMine.jsx
@@ -78,21 +78,81 @@ function add_keys(Info,ItemList) {
 	}
 }
 
+///////// Camera-model bits ////////////////////
+
+function camera_id(ModelName,info,descBits) {
+    if (ModelName == "DMC-LX1") {
+    	add_keys(info,["LX1","DMC_LX1"]);
+		descBits.lumix = true;
+		descBits.multiplier = 4.4;
+		descBits.camera = 'LX1';
+    } else if (ModelName == "DMC-LX2") {
+    	add_keys(info,["LX2","DMC_LX2"]);
+		descBits.lumix = true;
+		descBits.multiplier = 4.4;
+		descBits.camera = 'LX2';
+    } else if (ModelName == "DMC-LX3") {
+    	add_keys(info,["LX3","DMC_LX3"]);
+    	//add_keys(info,["Bike","Bicycle"]);
+		descBits.lumix = true;
+		descBits.multiplier = 4.67;
+		descBits.camera = 'LX3';
+    } else if (ModelName == "DMC-LX5") {
+    	add_keys(info,["LX5","DMC_LX5"]);
+		descBits.lumix = true;
+		descBits.multiplier = 4.67;
+		descBits.camera = 'LX5';
+    } else if (ModelName == "DMC-LX7") {
+    	add_keys(info,["LX7","DMC_LX7"]);
+		descBits.lumix = true;
+		descBits.multiplier = 4.67;
+		descBits.camera = 'LX7';
+    } else if (ModelName == "X100S") {
+    	add_keys(info,["Fuji X100s","X100s"]);
+		descBits.fujix = true;
+		descBits.multiplier = (35.0/23.0);
+		descBits.camera = 'X100s';
+    } else if (ModelName == "X-T1") {
+    	add_keys(info,["Fuji X-T1","X-T1"]);
+		descBits.fujix = true;
+		descBits.multiplier = (35.0/23.0);
+		descBits.camera = 'X-T1';
+    } else if (ModelName == "Canon EOS 5D") {
+    	add_keys(info,["5D","EOS","Canon 5D"]);
+	    descBits.multiplier = 1.0;
+		descBits.camera = '5D';
+    } else if (ModelName == "Canon EOS 40D") {
+    	add_keys(info,["40D","EOS","Canon 40D"]);
+	    descBits.multiplier = 1.6;
+		descBits.camera = '40D';
+    } else if (ModelName == "Canon EOS DIGITAL REBEL") {
+    	add_keys(info,["300D","EOS"]);
+	    descBits.multiplier = 1.6;
+		descBits.camera = '300D';
+    } else if (ModelName == "Glass1") {
+    	add_keys(info,["Google","Glass","Google Glass","Android"]);
+	    descBits.multiplier = 8.0;
+		descBits.camera = 'Google Glass';
+    } else {
+		if (descBits.alertText != "") {
+			descBits.alertText += "\n";
+		}
+		descBits.alertText += ("Model: \"" + ModelName + "\"");
+		descBits.camera = ('Camera: '+ModelName);
+		// alert("Model: \""+q[1]+"\"");
+    }
+	return info;
+}
 
 ////////////// march through EXIF tags //////////////
 
 function scan_exif_stuff(doc)
 {
     var info = doc.info;
-    var digiCam = false;
-	var lumix = false;
-	var fujix = false;
-    var blah = "EXIF:\n";
     var FL = 0;
     var oFL = 0;
-    var multiplier = 1.6;
-    var used = false;
-    var debugEXIF = false;
+    var fls;
+    var debugMsg = false;
     var descBits = {
     	camera: 'Scanned',
     	lens: '',
@@ -100,122 +160,66 @@ function scan_exif_stuff(doc)
     	aperture: '',
     	iso: '',
     	flash: '',
-    	exifMsgs: ''
+    	alertText: '',
+    	multiplier: 1.6,
+    	fujix: false,
+    	lumix: false
     };
-    var fls;
     for (var i = 0; i < info.exif.length; i++) {
 		q = info.exif[i];
-		blah += q[0] + ": " + q[1] + "\n";
 		if (q[0] == "Make") {
 		    info.keywords = Set.add(info.keywords, q[1]);
-		    digiCam = true;
-		    used = true;
+		} else if (q[0] == "Model") {
+			camera_id(q[1],info,descBits);  // identify specific model of camera
+		} else if ((q[0] == "Date Time") ||
+					(q[0] == "Date Time Original")) {
+		    info.keywords = Set.add(info.keywords,q[1].substr(0,4));
+		} else if (q[0] == "Focal Length in 35mm Film") {
+		    FL = 0 + q[1];
+		} else if (q[0] == "Shutter Speed") {
+			descBits.shutter = (' - '+q[1]);
+		} else if (q[0] == "Focal Length") {
+		    oFL = 0 + q[1];
+		    fls = (Math.floor(oFL+0.49)).toString();
+			descBits.lens = (', '+fls+'mm');
+		} else if (q[0] == "F-Stop") {
+			descBits.aperture = (' '+q[1]);
+		} else if (q[0] == "ISO Speed Ratings") {
+			descBits.iso = (', ISO '+q[1]);
+		} else if (q[0] == "Copyright") {
+			if (q[1].match(/[0-9]/)) {
+				descBits.alertText += ("\nEXIF Copyright Notice: \""+q[1]+"\"");
+			}
+		} else if (q[0] == "Scene Capture Type") {
+			if (q[1] != "Normal") {
+			    info.keywords = Set.add(info.keywords, q[1]);
+			}
+		} else if (q[0] == "Light Source") {
+			if (q[1] != "Unknown") {
+			    info.keywords = Set.add(info.keywords, q[1]);
+			}
 		} else if (q[0] == "Flash") {
-		    // alert("Flash ["+q[1]+"]");
-		    var flashVal = 0 + q[1]
+		    var flashVal = 0 + q[1];
 		    if (flashVal < 16) {
-				blah += "STROBE FIRED\n";
 				info.keywords = Set.add(info.keywords, "Strobe");
 				descBits.flash = "+ Flash";
 		    }
-		    used = true;
 		} else if (q[0] == "Scene Type") {
 		    info.source = q[1];
-		    used = true;
 		} else if (q[0] == "Custom Rendered") {
 		    if (q[1] == "Custom Process") {
 				info.keywords = Set.add(info.keywords, "BW");
 		    }
-		    used = true;
-		} else if (q[0] == "Model") {
-			used = true;
-		    if (q[1] == "DMC-LX1") {
-		    	add_keys(info,["LX1","DMC_LX1"]);
-				lumix = true;
-				multiplier = 4.4;
-				descBits.camera = 'LX1';
-		    } else if (q[1] == "DMC-LX2") {
-		    	add_keys(info,["LX2","DMC_LX2"]);
-				lumix = true;
-				multiplier = 4.4;
-				descBits.camera = 'LX2';
-		    } else if (q[1] == "DMC-LX3") {
-		    	add_keys(info,["LX3","DMC_LX3"]);
-		    	//add_keys(info,["Bike","Bicycle"]);
-				lumix = true;
-				multiplier = 4.67;
-				descBits.camera = 'LX3';
-		    } else if (q[1] == "DMC-LX5") {
-		    	add_keys(info,["LX5","DMC_LX5"]);
-				lumix = true;
-				multiplier = 4.67;
-				descBits.camera = 'LX5';
-		    } else if (q[1] == "DMC-LX7") {
-		    	add_keys(info,["LX7","DMC_LX7"]);
-				lumix = true;
-				multiplier = 4.67;
-				descBits.camera = 'LX7';
-		    } else if (q[1] == "X100S") {
-		    	add_keys(info,["Fuji","Fujifilm","Fuji X","Fuji X100s","X100s"]);
-				lumix = false;
-				fujix = true;
-				multiplier = (35.0/23.0);
-				descBits.camera = 'X100s';
-		    } else if (q[1] == "X-T1") {
-		    	add_keys(info,["Fuji","Fujifilm","Fuji X","Fuji X-T1","X-T1"]);
-				lumix = false;
-				fujix = true;
-				multiplier = (35.0/23.0);
-				descBits.camera = 'X-T1';
-		    } else if (q[1] == "Canon EOS 5D") {
-		    	add_keys(info,["5D","EOS","Canon 5D"]);
-			    digiCam = true;
-			    multiplier = 1.0;
-				descBits.camera = '5D';
-		    } else if (q[1] == "Canon EOS 40D") {
-		    	add_keys(info,["40D","EOS","Canon 40D"]);
-			    digiCam = true;
-			    multiplier = 1.6;
-				descBits.camera = '40D';
-		    } else if (q[1] == "Canon EOS DIGITAL REBEL") {
-		    	add_keys(info,["300D","EOS"]);
-			    multiplier = 1.6;
-				descBits.camera = '300D';
-		    } else if (q[1] == "Glass1") {
-		    	add_keys(info,["Google","Glass","Google Glass","Android"]);
-			    multiplier = 8.0;
-				descBits.camera = 'Google Glass';
-		    } else {
-				if (descBits.exifMsgs != "") {
-					descBits.exifMsgs += "\n";
+		} else if (q[0] == "Artist") {
+		    if (q[1] != "Kevin Bjorke") {
+				if (descBits.alertText != "") {
+					descBits.alertText += "\n";
 				}
-				descBits.exifMsgs += ("Model: \"" + q[1] + "\"");
-				descBits.camera = ('Camera: '+q[1]);
-				// alert("Model: \""+q[1]+"\"");
+				descBits.alertText += ("Artist tag: \""+q[1]+"\"");
+				// alert("Artist tag: \""+q[1]+"\"");
 		    }
-		} else if ((q[0] == "Date Time") ||
-					(q[0] == "Date Time Original")) {
-		    info.keywords = Set.add(info.keywords,q[1].substr(0,4));
-		    used = true;
-		} else if (q[0] == "Focal Length in 35mm Film") {
-		    // alert("35mm FL was ["+q[1]+"]");
-		    FL = 0 + q[1];
-		    used = true;
-		} else if (q[0] == "Orientation") {
-		} else if (q[0] == "Shutter Speed") {
-				descBits.shutter = (' - '+q[1]);
-		} else if (q[0] == "Focal Length") {
-		    // alert("Base FL was ["+q[1]+"]");
-		    oFL = 0 + q[1];
-		    fls = (Math.floor(oFL+0.49)).toString();
-			descBits.lens = (', '+fls+'mm');
-			used = true;
-		} else if (q[0] == "Metering Mode") {
-		} else if (q[0] == "Copyright") {
-			if (q[1].match(/[0-9]/)) {
-				descBits.exifMsgs += ("\nEXIF Copyright Notice: \""+q[1]+"\"");
-			}
-			used = true;
+		} else if (q[0] == "Metering Mode") { debugMsg=true;
+		} else if (q[0] == "Orientation") { debugMsg=true;
 		} else if (q[0] == "Color Space") {
 		} else if (q[0] == "Pixel X Dimension") {
 		} else if (q[0] == "Pixel Y Dimension") {
@@ -229,25 +233,12 @@ function scan_exif_stuff(doc)
 		} else if (q[0] == "Resolution Unit") {
 		} else if (q[0] == "yCbCr Positioning") {
 		} else if (q[0] == "Exposure Time") {
-				//descBits.shutter = (descBits.shutter+'- T'+q[1]);
-		} else if (q[0] == "F-Stop") {
-			descBits.aperture = (' '+q[1]);
-			used = true;
 		} else if (q[0] == "Aperture Value") {
-				//descBits.aperture = (descBits.aperture+'@ f'+q[1]);
 		} else if (q[0] == "Max Aperture Value") {
 		} else if (q[0] == "Exposure Bias Value") {
-		} else if (q[0] == "Exposure Mode") {
-		} else if (q[0] == "Exposure Program") {
+		} else if (q[0] == "Exposure Mode") { debugMsg=true;
+		} else if (q[0] == "Exposure Program") { debugMsg=true;
 		} else if (q[0] == "White Balance") {
-		} else if (q[0] == "Scene Capture Type") {
-			if (q[1] != "Normal") {
-			    info.keywords = Set.add(info.keywords, q[1]);
-			}
-			used = true;
-		} else if (q[0] == "ISO Speed Ratings") {
-			descBits.iso = (', ISO '+q[1]);
-			used = true;
 		} else if (q[0] == "Sensing Method") {
 		} else if (q[0] == "File Source") {
 		} else if (q[0] == "ExifVersion") {
@@ -256,11 +247,6 @@ function scan_exif_stuff(doc)
 		} else if (q[0] == "Software") {
 		} else if (q[0] == "Digital Zoom Ratio") {
 		} else if (q[0] == "Compressed Bits Per Pixel") {
-		} else if (q[0] == "Light Source") {
-			if (q[1] != "Unknown") {
-			    info.keywords = Set.add(info.keywords, q[1]);
-			}
-			used = true;
 		} else if (q[0] == "Gain Control") {
 		} else if (q[0] == "Contrast") {
 		} else if (q[0] == "Saturation") {
@@ -275,48 +261,29 @@ function scan_exif_stuff(doc)
 		} else if (q[0] == "EXIF tag 42036") { // X-T1: "XF18-55mmF2.8-4 R LM OIS"
 			if (q[1] = "XF18-55mmF2.8-4 R LM OIS") {
 			    info.keywords = Set.add(info.keywords, "18-55mm");
-			    // info.keywords = Set.add(info.keywords, "Zoom");
-			    // info.keywords = Set.add(info.keywords, "Kit Lens");
 			}
-			used = true;
 		} else if (q[0] == "EXIF tag 42037") { // X-T1: serial #
 		} else if (q[0] == "Brightness Value") { // first seen on x100s
 		} else if (q[0] == "Subject Distance Range") { // first seen on x100s
 		} else if (q[0] == "Subject Distance") { // first seen on Glass
 		} else if (q[0] == "Image Unique ID") { // first seen on Glass
-		} else if (q[0] == "Artist") {
-		    if (q[1] != "Kevin Bjorke") {
-				if (descBits.exifMsgs != "") {
-					descBits.exifMsgs += "\n";
-				}
-				descBits.exifMsgs += ("Artist tag: \""+q[1]+"\"");
-				// alert("Artist tag: \""+q[1]+"\"");
-		    }
-		    used = true;
 		} else {
-		    if (descBits.exifMsgs != "") {
-		    	descBits.exifMsgs += "\n";
-		    }
-		    descBits.exifMsgs += ("EXIF \""+q[0]+"\" was \""+q[1]+"\"");
-		    // alert("EXIF \""+q[0]+"\" was \""+q[1]+"\"");
+			debugMsg = true;
 		}
-		if (debugEXIF && (! used)) {
-			alert(q.toString());
-			used = false;
+		if (debugMsg) {
+		    if (descBits.alertText != "") {
+		    	descBits.alertText += "\n";
+		    }
+		    descBits.alertText += ('EXIF "'+q[0]+'" was "'+q[1]+'"');
+			debugMsg = false;
 		}
     }
     //
     //
-    if (lumix) {
-		    	add_keys(info,["Leica","Lumix"]);
-		if (doc.width > doc.height) {
-		    info.keywords = Set.add(info.keywords, "16_9");
-		} else {
-		    info.keywords = Set.add(info.keywords, "9_16");
-		}
-		digiCam = true;
-	} else if (fujix) {
-		digiCam = true;
+    if (descBits.lumix) {
+    	add_keys(info,["Leica","Lumix","Leicasonic","Panaleica"]);
+	} else if (descBits.fujix) {
+    	add_keys(info,["Fuji","Fujifilm","Fuji X"]);
 		if (oFL == 45) {
 			add_keys(info,["Zeiss","Contax","Planar","f/2","Fotodiox"]);
 		} else if (oFL == 90) {
@@ -327,13 +294,12 @@ function scan_exif_stuff(doc)
 			add_keys(info,["Rokinon","f/2.8"]);
 		}
 	}
-    if (! digiCam) {
+    if (descBits.camera == 'Scanned') { // never saw a camera
 		add_keys(info,["film","scanned"]);
     }
     if (FL <= 0) {
-		FL = oFL * multiplier;
+		FL = oFL * descBits.multiplier;
 		FL = Math.floor(FL+0.49);
-		//alert("Calculated FL was "+FL);
 	} else { // equivalent supplied by camera
 		fls = (Math.floor(oFL+0.49)).toString();
         if (fls.substr(0,1) == "0") {
@@ -342,14 +308,12 @@ function scan_exif_stuff(doc)
 		info.keywords = Set.add(info.keywords, (fls+"mm_orig"));
     }
     if (FL > 0) {
-		//alert("Final oFL was "+oFL);
-		//alert("Final FL was "+FL);
 		if (FL <= 35) {
 		    info.keywords = Set.add(info.keywords, "Wide_Angle");
 		} else if (FL >= 85) {
 		    info.keywords = Set.add(info.keywords, "Telephoto");
 		}
-	    if (multiplier != 1.0) {
+	    if (descBits.multiplier != 1.0) {
 	        fls = FL.toString();
 	        if (fls.substr(0,1) == "0") {
 	           fls = fls.substr (1);
@@ -358,9 +322,41 @@ function scan_exif_stuff(doc)
 	        info.keywords = Set.add(info.keywords, (fls+"mm_equiv"));
 		}
     }
-    // alert(blah);
     return descBits;
 }
+
+/////////////////////////////////////////////////////////////
+
+function aspect_desc(doc)
+{
+    var info = doc.info;
+    var a, l, s, w;
+    l = Math.max(doc.width,doc.height);
+    s = Math.min(doc.width,doc.height);
+    a = (l/s);
+    w = (doc.width > doc.height);
+    if (a>2.36) {
+    	info.keywords = Set.add(info.keywords, 'Panorama');
+    	if (!w) {
+	   		info.keywords = Set.add(info.keywords, 'Tall');
+	   	}
+    } else if (a>2.3) {
+    	info.keywords = Set.add(info.keywords, (w?'2.35_1':'1_2.35'));	
+    } else if (a>1.97) {
+    	info.keywords = Set.add(info.keywords, (w?'2_1':'1_2'));	
+    } else if (a>1.68) {
+	   	info.keywords = Set.add(info.keywords, (w?'16_9':'9_16'));	
+     } else if (a>1.54) {
+    	info.keywords = Set.add(info.keywords, (w?'3_2':'2_3');	
+    } else if (a>1.3) {
+    	info.keywords = Set.add(info.keywords, (w?'4_3':'3_4'));	
+    } else {
+    	info.keywords = Set.add(info.keywords, 'Square');	    	
+    	info.keywords = Set.add(info.keywords, '1_1');	    	
+    }
+}
+
+////////////////////////////////////////////
 
 function main()
 {
@@ -377,24 +373,17 @@ function main()
     var msgs = "";
     if (initKeys > 0) {
 		msgs = (msgs + initKeys.toString() + " keys already defined");
-		// alert(initKeys.toString()+" keys already defined");
     }
     var newKeys = [];
     if (app.activeDocument.mode == DocumentMode.GRAYSCALE) {
 		newKeys = newKeys.concat(["BW","Black_and_White","Black_&_White","Monochrome"]);
-		// info.keywords = Set.add(info.keywords, "bw");
-		// info.keywords = Set.add(info.keywords, "black_and_white");
-		// info.keywords = Set.add(info.keywords, "monochrome");
     }
     var dt = new Date();
     var thisYear = dt.getFullYear()
     var thisYearS = thisYear.toString()
     if (info.CreationDate == "") {
 		info.creationDate = dt.toString();
-	//    } else {
-	//	alert("Image Created <"+info.creationDate+">");
     }
-    // alert("Title is ("+info.title+")");
     newKeys = newKeys.concat(["Kevin_Bjorke","Bjorke","PhotoRant","Botzilla.com",
 	    						"San_Francisco","SF"/*,"Santa_Clara"*/,"Bay_Area","California"]);
     //
@@ -402,9 +391,10 @@ function main()
     //
     add_keys(info,newKeys);
     var descBits = scan_exif_stuff(app.activeDocument);
-    if (descBits.exifMsgs != "") {
+    aspect_desc(app.activeDocument);
+    if (descBits.alertText != "") {
 		if (msgs != "") { msgs += "\n"; }
-		msgs += descBits.exifMsgs;
+		msgs += descBits.alertText;
     }
     info.author = "Kevin Bjorke";
     info.credit = "Kevin Bjorke";
@@ -430,7 +420,7 @@ function main()
 		info.captionWriter = "Kevin Bjorke";
     }
     if (info.city == "") {info.city = "San Francisco"; }
-    if (info.provineState == "") {info.provinceState = "California"; }
+    if (info.provinceState == "") {info.provinceState = "California"; }
     if (info.country == "") { info.country == "USA"; }
     if (initKeys == 0) {
 		info.keywords = Set.add(info.keywords, "needs_tags");

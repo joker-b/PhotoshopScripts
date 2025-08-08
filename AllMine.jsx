18e5a4fa3433f29c040d650e8cd5bba6f114d4a9
Kevin Bjorke <kevin.bjorke+dev@gmail.com>
2025-08-07 18:54:55 -0700

    only comment changes

--- AllMine.jsx
+++ AllMine.jsx
@@ -674,11 +674,11 @@ var LensCatalog = {
     //
     // Various Adapted Remaps and shorthand names
     //
-    // Zeiss-specified Leica profiles for ZM lenses
-    'Elmarit-M 1:2.8/28': { remap: 'Zeiss Biogon T* 2,8/21 ZM' }, // 11809
-    'Summicron-M 1:2/28 ASPH.': { remap: 'Zeiss Biogon T* 2,8/28 ZM' }, // 11604 f/2 !
-    'Elmarit-M 1:2.8/28 ASPH.': { remap: 'Zeiss Biogon T* 2/35 ZM' }, // 11606
-    'Summilux-M 1:1.4/35 ASPH.': { remap: 'Zeiss Distagon T* 1,4/35 ZM', }, // 11873
+    // ZEISS-specified Leica profiles for ZM lenses 
+    'Elmarit-M 1:2.8/28': { remap: 'Zeiss Biogon T* 2,8/21 ZM' },           // 28 f/2.8 11809
+    'Summicron-M 1:2/28 ASPH.': { remap: 'Zeiss Biogon T* 2,8/28 ZM' },     // 28 f/2 11604 yes f/2 !
+    'Elmarit-M 1:2.8/28 ASPH.': { remap: 'Zeiss Biogon T* 2/35 ZM' },       // 28 f/28 11606
+    'Summilux-M 1:1.4/35 ASPH.': { remap: 'Zeiss Distagon T* 1,4/35 ZM', }, // 35 f/14 ASPH 11873
     //
     'Elmarit-M 1:2.8/90': { remap: 'Tele-Elmarit 1:2.8/90' },
     'Summilux-M 1:1.4/50 ASPH.': { remap: 'TTArtisans-M 1:1.4/50 ASPH.', },
